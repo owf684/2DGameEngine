6b9dcc638a395cccd6ac7e11d5bae43fe9dde8a3
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-09-17 22:50:32 -0500

    added top,bottomleft,right collisions and changed jumping physics

--- CollisionEngine/CollisionEngine.py
+++ CollisionEngine/CollisionEngine.py
@@ -20,7 +20,6 @@ class _CollisionEngine:
 
 
 		numObjects = len(collisionBuffer)
-		print(numObjects)
 		while currentObject < numObjects:
 
 			self.updateRectPosition(collisionBuffer[currentObject])
@@ -28,7 +27,8 @@ class _CollisionEngine:
 			#Reset collisions here please!
 			collisionBuffer[currentObject].collisionDown = False
 			collisionBuffer[currentObject].collisionLeft = False
-
+			collisionBuffer[currentObject].collisionRight = False
+			collisionBuffer[currentObject].collisionUp = False
 
 			for objects in collisionBuffer:
 				self.updateRectPosition(objects)
@@ -42,9 +42,9 @@ class _CollisionEngine:
 		
 						self.handle_left_collisions(collisionBuffer,objects,currentObject)
 						
+						self.handle_right_collisions(collisionBuffer,objects,currentObject)
 
-
-
+						self.handle_up_collisions(collisionBuffer,objects,currentObject)
 
 
 			currentObject += 1
@@ -53,63 +53,136 @@ class _CollisionEngine:
 
 		if collisionBuffer[currentObject].rect.colliderect(objects.rect) :
 
-			if collisionBuffer[currentObject].rect.bottom > objects.rect.top:
+			if collisionBuffer[currentObject].rect.midbottom[0] > objects.rect.topleft[0] and collisionBuffer[currentObject].rect.midbottom[0] < objects.rect.topright[0]:
 
-				if not collisionBuffer[currentObject].rect.centery > objects.rect.top and not collisionBuffer[currentObject].rect.centery < objects.rect.bottom:
+				if collisionBuffer[currentObject].rect.midtop[1] < objects.rect.centery:
 
-					collisionBuffer[currentObject].collisionDown = True
-					collisionBuffer[currentObject].position[1] = objects.rect.top - collisionBuffer[currentObject].rect.height
+					if collisionBuffer[currentObject].rect.bottom > objects.rect.top:
+
+						collisionBuffer[currentObject].collisionDown = True
 
 
-		print ("collisonBuffer.collisionDown: " + str(collisionBuffer[currentObject].collisionDown))
+			#if collisionBuffer[currentObject].rect.bottomright[0] > objects.rect.topleft[0] and collisionBuffer[currentObject].rect.bottomright[0] < objects.rect.midtop[0]:
+
+			#	if collisionBuffer[currentObject].rect.midtop[1] < objects.rect.midbottom[1]:
+
+			#		if collisionBuffer[currentObject].rect.bottom > objects.rect.top:
+
+			#			collisionBuffer[currentObject].collisionDown = True
+
+
+			#if collisionBuffer[currentObject].rect.bottomleft[0] > objects.rect.midtop[0] and collisionBuffer[currentObject].rect.bottomleft[0] < objects.rect.topright[0]:
+			#	if collisionBuffer[currentObject].rect.bottom > objects.rect.top:
+
+			#		collisionBuffer[currentObject].collisionDown = True
+
+			if collisionBuffer[currentObject].collisionDown:
+						collisionBuffer[currentObject].position[1] = objects.rect.top - collisionBuffer[currentObject].rect.height
+
 
 	def handle_left_collisions(self,collisionBuffer,objects,currentObject):
 
-		'''
-		scenario 1	
-					 ___
-		object	-->	|	|  ___
-					|___| |   | <-- player
-						  |___|				  
 
-		'''
-		if collisionBuffer[currentObject].rect.top > objects.rect.top and collisionBuffer[currentObject].rect.top < objects.rect.bottom:
+		if collisionBuffer[currentObject].rect.colliderect(objects.rect):
 
-			if collisionBuffer[currentObject].rect.left < objects.rect.right:
+			'''
+			scenario 1	
+						 ___
+			object	-->	|	|  ___
+						|___| |   | <-- player
+							  |___|				  
 
-					collisionBuffer[currentObject].collisionLeft = True
-					print("scenario 1")
-		'''
-		scenario 2	 __    ___
-		object 	--> |	| |   |  <-- player 
-					|___| |___| 
-						  
-		'''					
-		if collisionBuffer[currentObject].rect.centery == objects.rect.centery:
+			'''
+			if collisionBuffer[currentObject].rect.topleft[1] > objects.rect.midright[1] and collisionBuffer[currentObject].rect.topleft[1] < objects.rect.bottomright[1]:
+		
+				if collisionBuffer[currentObject].rect.midright[0] > objects.rect.centerx:
 
-				if collisionBuffer[currentObject].rect.left < objects.rect.right:
-
-					if collisionBuffer[currentObject].rect.right > objects.rect.left:
-
+					if collisionBuffer[currentObject].rect.left < objects.rect.right:
 						collisionBuffer[currentObject].collisionLeft = True
-						print("scenario 2")
-		'''
-		scenario 3		   ___
-					 ___  |	  |	<-- player
-		object 	--> |	| |___|
-					|___|
-		'''			
-		if collisionBuffer[currentObject].rect.bottom < objects.rect.bottom  and collisionBuffer[currentObject].rect.bottom - 5 > objects.rect.top:
+						return None
+			'''d
+			scenario 2	 __    ___
+			object 	--> |	| |   |  <-- player 
+						|___| |___| 
+							  
+			'''					
+			if objects.rect.topright[1] < collisionBuffer[currentObject].rect.midleft[1] and collisionBuffer[currentObject].rect.midleft[1] < objects.rect.bottomright[1]:
+				if collisionBuffer[currentObject].rect.midright[0] > objects.rect.centerx:
 
-			if collisionBuffer[currentObject].rect.left < objects.rect.right and collisionBuffer[currentObject].rect.right > objects.rect.right:
+					if collisionBuffer[currentObject].rect.left > objects.rect.left:
+					
+						if collisionBuffer[currentObject].rect.left < objects.rect.right:
 
-					collisionBuffer[currentObject].collisionLeft = True
-					print("player_bottom: " + str(collisionBuffer[currentObject].rect.bottom))
-					print("objects_bottom: " + str(objects.rect.bottom))
-					print("objects_top: " + str(objects.rect.top))
-					print ("scenario 3")
+							collisionBuffer[currentObject].collisionLeft = True
+							return None
+
+			'''
+			scenario 3		   ___
+						 ___  |	  |	<-- player
+			object 	--> |	| |___|
+						|___|
+			'''			
+			if collisionBuffer[currentObject].rect.bottomleft[1] > objects.rect.topright[1]+10 and collisionBuffer[currentObject].rect.bottomleft[1] < objects.rect.midright[1]:
+
+				if collisionBuffer[currentObject].rect.midright[0] > objects.rect.centerx:
+
+					if collisionBuffer[currentObject].rect.left < objects.rect.right:
+						collisionBuffer[currentObject].collisionLeft = True
+						return None
+
+	def handle_right_collisions(self,collisionBuffer,objects,currentObject):
 
 
+		if collisionBuffer[currentObject].rect.colliderect(objects.rect):
+
+			'''
+			scenario 1	
+						 ___
+			player	-->	|	|  ___
+						|___| |   | <-- object
+							  |___|				  
+
+			'''
+			if collisionBuffer[currentObject].rect.bottomright[1] > objects.rect.topleft[1] + 10 and collisionBuffer[currentObject].rect.bottomright[1] < objects.rect.bottomleft[1]:
+				if collisionBuffer[currentObject].rect.midleft[0] < objects.rect.midright[0]:
+					if collisionBuffer[currentObject].rect.midright[0] > objects.rect.centerx:
+						collisionBuffer[currentObject].collisionRight = True
+			'''
+			scenario 2	 __    ___
+			player 	--> |	| |   |  <-- object 
+						|___| |___| 
+							  
+			'''					
+			if objects.rect.topleft[1] < collisionBuffer[currentObject].rect.midright[1] and collisionBuffer[currentObject].rect.midright[1] < objects.rect.bottomleft[1]:
+
+				if collisionBuffer[currentObject].rect.midleft[0] < objects.rect.centerx:
+					print(collisionBuffer[currentObject].rect.midleft)
+					print(objects.rect.midright)
+					if collisionBuffer[currentObject].rect.right > objects.rect.left:
+						collisionBuffer[currentObject].collisionRight = True
+
+			'''
+			scenario 3		   ___
+						 ___  |	  |	<-- object
+			player 	--> |	| |___|
+						|___|
+			'''			
+			if collisionBuffer[currentObject].rect.topright[1] > objects.rect.midleft[1] and collisionBuffer[currentObject].rect.topright[1] < objects.rect.bottomleft[1]:
+				if collisionBuffer[currentObject].rect.midleft[0] < objects.rect.centerx:
+					if collisionBuffer[currentObject].rect.right > objects.rect.left:
+						collisionBuffer[currentObject].collisionRight = True
+
+	def handle_up_collisions(self,collisionBuffer,objects,currentObject):
+
+
+		if collisionBuffer[currentObject].rect.colliderect(objects.rect):
+
+			if collisionBuffer[currentObject].rect.midtop[0] > objects.rect.bottomleft[0] and collisionBuffer[currentObject].rect.midtop[0] < objects.rect.bottomright[0]:
+				if objects.rect.centery < collisionBuffer[currentObject].rect.midbottom[1]:
+
+					if collisionBuffer[currentObject].rect.top < objects.rect.bottom:
+						collisionBuffer[currentObject].collisionUp = True
+		#print(collisionBuffer[currentObject].collisionUp)
 	def updateRectPosition(self,collisionObject):
 
 		collisionObject.rect.x = collisionObject.position[0]
