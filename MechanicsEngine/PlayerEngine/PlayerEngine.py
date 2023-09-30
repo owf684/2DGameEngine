7b584fbb7c31289cfcb306746eafddc3a2c39c28
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-09-29 22:40:16 -0500

    minor improvements to physics and collisions

--- MechanicsEngine/PlayerEngine/PlayerEngine.py
+++ MechanicsEngine/PlayerEngine/PlayerEngine.py
@@ -35,44 +35,28 @@ class _PlayerEngine:
 	def onEnemy(self,objects):
 		if objects.subClass =='player':
 			if objects.onEnemy:
-				objects.jump_velocity_1 = 250
-				objects.onEnemy = False
-
-	def jump(self,objects,delta_t,input_dict):
-			#Handle jump velocity
-			if input_dict['up'] == '1' and not objects.collisionUp and not self.reached_max_height:
+				objects.velocityY = 250
 				objects.jumping = True
-				objects.jump_velocity_1 = 450
+				objects.onEnemy = False
+	def jump(self,objects,delta_t,input_dict):
+		if input_dict['up'] == '1' and not self.reached_max_height:
+			objects.velocityY = 350
+			objects.jumping = True
 
-			elif input_dict['up'] == '0' or self.reached_max_height:
-				objects.jump_velocity_1 -= 15
+		self.total_y_displacement += objects.y_displacement
+		if self.total_y_displacement >= 80:
+			self.reached_max_height = True
 
-			if objects.collisionDown:
+		if objects.collisionUp:
+			self.reached_max_height = True
+			objects.velocityY *= -1
 
+		if objects.collisionDown:
+			self.total_y_displacement = 0
+			objects.jumping = False
+			if self.reached_max_height:
 				if input_dict['up'] == '0':
-					self.total_y_displacement = 0
-					objects.jumping = False
 					self.reached_max_height = False
-
-			if self.total_y_displacement > 100:
-				self.reached_max_height = True
-
-			if objects.collisionUp and not self.reached_max_height:
-				self.total_y_displacement = 0
-				objects.jumping = False
-				objects.jump_velocity_1 = 0
-				self.reached_max_height = True
-			if objects.jump_velocity_1 < 0:
-				objects.jump_velocity_1 = 0
-			#handle position calculations
-			if delta_t != 0:
-				self.y_displacement	= objects.jump_velocity_1*delta_t + (0.5 * (objects.jump_velocity_1/delta_t) * math.pow(delta_t,2) )
-				if self.y_displacement < 0:
-					self.y_displacement = 0
-				objects.position[1] -= self.y_displacement
-				objects.rect.y -= self.y_displacement
-				self.total_y_displacement += self.y_displacement
-
 
 	def horizontal_movement(self,objects,delta_t,input_dict,CollisionEngine,levelHandler):
 
@@ -88,7 +72,7 @@ class _PlayerEngine:
 			objects.velocityX = 0
 		
 		if input_dict['l-shift'] == '1':
-			self.runningFactor = 2
+			self.runningFactor = 1.5
 		else:
 			self.runningFactor = 1
 
