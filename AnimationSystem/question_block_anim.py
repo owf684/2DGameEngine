0ca49dff55c2b575afa5af52d4fd97499fec3312
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-09-30 14:38:02 -0500

    updated question block animation to conform with the new animation system

--- AnimationSystem/question_block_anim.py
+++ AnimationSystem/question_block_anim.py
@@ -1,17 +1,14 @@
 import pygame
+import anim_util
 import copy
 
-class _question_block:
+class _question_block(anim_util._anim_util):
 
     def __init__(self):
-        self.current_time = 0
-        self.elapsed_time = 0
-        self.last_frame_time = 0
-        self.frame_index = 0
+        super().__init__()
         self.frame_size = (32, 32)
         self.frame_count = 3
         self.frame_duration = 200
-
         self.question_block_sprite_sheet = './Assets/Platforms/question_block_states/question_block_32x32_sprite_sheet.png'
         self.question_block_frames = self.extract_frames(self.question_block_sprite_sheet,self.frame_count,self.frame_size[0],self.frame_size[1])
 
@@ -21,22 +18,5 @@ class _question_block:
     def question_block_animation(self, levelHandler):
         for qb in levelHandler.question_blocks:
             if not qb.hit:
-
                 qb.image = self.question_block_frames[self.frame_index]
 
-    def determine_frame_count(self):
-
-        self.current_time = pygame.time.get_ticks()
-        self.elapsed_time = self.current_time - self.last_frame_time
-
-        if self.elapsed_time >= self.frame_duration:
-            self.frame_index = (self.frame_index + 1) % self.frame_count
-            self.last_frame_time = self.current_time
-
-    def extract_frames(self, path, num_frames, frame_width, frame_height):
-        frames = list()
-        sprite_sheet = pygame.image.load(path)
-        for i in range(num_frames):
-            frame = sprite_sheet.subsurface((0, i * frame_width, frame_width, frame_height))
-            frames.append(frame)
-        return frames
