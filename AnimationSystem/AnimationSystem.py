5f9039ef0fc40fc4c9bdf29e6ca35a685718ac7f
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-10-17 22:08:35 -0500

    modified animation system and goomba anim with enemyengine args

--- AnimationSystem/AnimationSystem.py
+++ AnimationSystem/AnimationSystem.py
@@ -14,11 +14,11 @@ class _AnimationSystem:
         self.firepower_anim = firepower_anim._firepower_anim()
         self.coin_anim = coin_anim._coin_anim()
         self.reset_animations = False
-    def main_loop(self, objects, GameObjects, levelObjects, input_dict, levelHandler, delta_t, PlayerEngine, GraphicsEngine):
+    def main_loop(self, objects, GameObjects, levelObjects, input_dict, levelHandler, delta_t, PlayerEngine, GraphicsEngine, EnemyEngine):
 
         self.question_block.main_loop(levelHandler)
         self.mario_anim.main_loop(self, objects, input_dict, levelHandler, delta_t,PlayerEngine)
-        self.goomba_anim.main_loop(objects)
+        self.goomba_anim.main_loop(objects, EnemyEngine)
         self.breakable_brick_anim.main_loop(objects,GameObjects, levelObjects, PlayerEngine, GraphicsEngine)
         self.firepower_anim.main_loop(objects,levelHandler,delta_t,PlayerEngine)
         self.coin_anim.main_loop(objects)
