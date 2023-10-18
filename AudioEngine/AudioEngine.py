12da6b02dc00a5b10747f1499d997f3fe402a80e
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-10-17 22:09:38 -0500

    added enemyFX to AudioEngine

--- AudioEngine/AudioEngine.py
+++ AudioEngine/AudioEngine.py
@@ -1,13 +1,16 @@
 import pygame
 import MarioFX
 import BlockFX
+import EnemyFX
 
 class _AudioEngine:
 
     def __init__(self):
         self.MarioFX = MarioFX._MarioFX()
         self.BlockFX = BlockFX._BlockFX()
-    def main_loop(self,objects,levelHandler,PlayerEngine):
+        self.EnemyFX = EnemyFX._EnemyFX()
+    def main_loop(self,objects,levelHandler,PlayerEngine, EnemyEngine):
         
         self.MarioFX.main_loop(objects,levelHandler,PlayerEngine)
         self.BlockFX.main_loop(objects,levelHandler,PlayerEngine)
+        self.EnemyFX.main_loop(objects,levelHandler,EnemyEngine)
