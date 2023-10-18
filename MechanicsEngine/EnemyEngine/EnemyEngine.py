ecb401e6f4778008af39222579e937e3ad729e91
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-10-17 22:10:52 -0500

    refactored goomba out of enemyengine. now calls goomba main_loop

--- MechanicsEngine/EnemyEngine/EnemyEngine.py
+++ MechanicsEngine/EnemyEngine/EnemyEngine.py
@@ -2,46 +2,21 @@ import pygame
 import copy
 import threading
 import sys
+import Goomba
 sys.path.append("./GameObjects")
 import BlockObject
 
 class _EnemyEngine():
 
     def __init__(self):
-        self.player_position = [0,0]
-        self.player_direction = 1
+        self.Goomba = Goomba._Goomba()
+
+        self.triggerStompAudio = False
 
     def main_loop(self, GameObjects, PlayerEngine, GraphicsEngine, objects):
-        self.get_player_attr(objects)
+        self.Goomba.main_loop(GameObjects,PlayerEngine,GraphicsEngine,objects,self)
 
-        if not objects.timer_started:
 
-            self.move_enemy(GameObjects, PlayerEngine, GraphicsEngine, objects)
-            self.enemy_ai(GameObjects, PlayerEngine, GraphicsEngine, objects)
-        
-        self.isHit(objects, GameObjects, GraphicsEngine)
-    
-    def get_player_attr(self,objects):
-        if objects.subClass == 'player':
-            self.player_position = objects.position
-            self.player_direction = objects.x_direction
-    def move_enemy(self, GameObjects, PlayerEngine, GraphicsEngine, objects):
-
-        if objects.subClass == 'enemy':
-
-            if objects.isRendered:
-                self.isHit(objects, GameObjects, GraphicsEngine)
-                self.change_direction(objects)
-                self.change_position(objects)
-
-    def isHit(self, objects, GameObjects, GraphicsEngine):
-        if objects.destroy and objects.subClass == 'enemy':
-            objects.destroy = False
-            GameObjects.remove(objects)
-            GraphicsEngine.render_buffer.remove(objects)
-
-    def change_position(self, objects):
-        objects.velocityX = 35
 
     def track_scroll(self, GameObjects, PlayerEngine):
         for objects in GameObjects:
@@ -49,32 +24,3 @@ class _EnemyEngine():
             if PlayerEngine.scroll_level and objects.subClass == 'enemy':
 
                 objects.position[0] -= PlayerEngine.x_displacement
-
-    def change_direction(self, objects):
-        if objects.collisionObject is not None:
-
-            if objects.collisionObject.subClass != "player":
-
-                if objects.collisionLeft:
-                    objects.x_direction = 1
-                if objects.collisionRight:
-                    objects.x_direction = -1
-            if isinstance(objects.collisionObject,BlockObject._BlockObject):
-                if objects.collisionObject.changeHit:
-                    objects.isHit = True 
-                    objects.fromUnder = True
-                    objects.collisionObject.changeHit = False
-            
-
-    def enemy_ai(self, GameObjects, PlayerEngine, GraphicsEngine, objects):
-        inRangeX = 128
-        inRangeY = 32
-        x_distance = objects.position[0] - self.player_position[0]
-        y_distance = objects.position[1] - self.player_position[1]
-        if objects.subClass == 'enemy':
-            if abs(x_distance) < inRangeX and abs(y_distance) <= inRangeY:
-                if x_distance < 0:    
-                    objects.x_direction = 1
-                if x_distance > 0:
-                    objects.x_direction = -1
-
