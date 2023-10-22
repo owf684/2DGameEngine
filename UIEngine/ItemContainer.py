1f06495c02be0b59ac75619cd408d05ed328cdb0
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-10-22 00:47:21 -0500

    added rect to ItemContainer

--- UIEngine/ItemContainer.py
+++ UIEngine/ItemContainer.py
@@ -12,9 +12,10 @@ class _ItemContainer:
         self.isActive = False
         self.active_image = None
         self.position = [0,0]
+        self.item_position = [0,0]
         self.item_image = None
         self.item_image_path = None
-
+        self.rect = None
 
 
     def set_active_image(self,active_image_path):
@@ -24,4 +25,6 @@ class _ItemContainer:
     def set_inactive_image(self, inactive_image_path):
         self.inactive_image_path = inactive_image_path
         self.item_inactive_image = pygame.image.load(self.inactive_image_path)
-
+    
+    def set_rect(self):
+	    self.rect = pygame.Rect(self.position[0],self.position[1],self.item_selected_image.get_width(),self.item_selected_image.get_height())        
