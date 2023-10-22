585ce5ef9910c8332f647daa0aa628b3fb5d64db
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-10-22 15:11:05 -0500

    added koopa anim to AnimationSystem

--- AnimationSystem/AnimationSystem.py
+++ AnimationSystem/AnimationSystem.py
@@ -4,6 +4,7 @@ import goomba_anim
 import breakable_brick_anim
 import firepower_anim
 import coin_anim
+import koopa_anim
 class _AnimationSystem:
 
     def __init__(self):
@@ -14,6 +15,7 @@ class _AnimationSystem:
             self.breakable_brick_anim = breakable_brick_anim._breakable_break_anim()
             self.firepower_anim = firepower_anim._firepower_anim()
             self.coin_anim = coin_anim._coin_anim()
+            self.koopa_anim = koopa_anim._koop_anim()
             self.reset_animations = False
         except Exception as Error:
             print("ERROR::AnimationSystem.py::__init__()", Error)
@@ -23,6 +25,7 @@ class _AnimationSystem:
             self.question_block.main_loop(levelHandler)
             self.mario_anim.main_loop(self, objects, input_dict, levelHandler, delta_t,PlayerEngine)
             self.goomba_anim.main_loop(objects, EnemyEngine)
+            self.koopa_anim.main_loop(objects,EnemyEngine)
             self.breakable_brick_anim.main_loop(objects,GameObjects, levelObjects, PlayerEngine, GraphicsEngine)
             self.firepower_anim.main_loop(objects,levelHandler,delta_t,PlayerEngine)
             self.coin_anim.main_loop(objects)
