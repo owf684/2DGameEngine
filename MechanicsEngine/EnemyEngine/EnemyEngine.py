9f8a43662e7e94dc579aaf6989be8cab03603af8
Christopher Corona <35810446+owf684@users.noreply.github.com>
2023-10-22 22:05:52 -0500

    added koopa engine

--- MechanicsEngine/EnemyEngine/EnemyEngine.py
+++ MechanicsEngine/EnemyEngine/EnemyEngine.py
@@ -3,6 +3,7 @@ import copy
 import threading
 import sys
 import Goomba
+import Koopa
 sys.path.append("./GameObjects")
 import BlockObject
 
@@ -10,11 +11,13 @@ class _EnemyEngine():
 
     def __init__(self):
         self.Goomba = Goomba._Goomba()
+        self.Koopa = Koopa._Koopa()
 
         self.triggerStompAudio = False
 
     def main_loop(self, GameObjects, PlayerEngine, GraphicsEngine, objects):
         self.Goomba.main_loop(GameObjects,PlayerEngine,GraphicsEngine,objects,self)
+        self.Koopa.main_loop(GameObjects,PlayerEngine,GraphicsEngine,objects,self)
 
 
 
